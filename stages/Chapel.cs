3ff00d92a2da080a81d32190864d41b1c8d4731f
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-07-24 19:42:25 -0300

    feat: add option to check the chapel

--- stages/Chapel.cs
+++ stages/Chapel.cs
@@ -20,12 +20,17 @@ namespace Reign_of_Grelok.stages
             Console.WriteLine("------------------------------------");
             Console.WriteLine("Pressione de acordo com o destino:");
             Console.WriteLine("1 - Olhar ao redor");
+
             if (stateManagementInstance.AlreadyCheckChapel())
             {
                 if ( !this.stateManagementInstance.AlreadyKilledZombie() ) Console.WriteLine("2 - Atacar zumbi com a espada");
                 Console.WriteLine("3 - Examinar cova");
             }
+
             Console.WriteLine("4 - Ir para Oeste");
+
+            if (this.stateManagementInstance.AlreadyUsedKey()) Console.WriteLine("5 - Examinar Capela");
+
             Console.WriteLine("I - Iventário");
             Console.WriteLine("Q - Sair");
             var keyInfo = Console.ReadKey();
@@ -62,6 +67,14 @@ namespace Reign_of_Grelok.stages
                     Console.Clear();
                     callback();
                     break;
+                case '5':
+                    this.CheckIfOptionIsAvailable(
+                            this.stateManagementInstance.AlreadyUsedKey(),
+                            callback
+                            );
+                    this.CheckChapel();
+                    this.Load(callback);
+                    break;
                 case 'q':
                 case 'Q':
                     Environment.Exit(0);
@@ -91,6 +104,18 @@ namespace Reign_of_Grelok.stages
 
         private void ShowStageMessage()
         {
+            if (this.stateManagementInstance.AlreadyKilledZombie() && this.stateManagementInstance.AlreadyUsedKey())
+            {
+                this.ShowPostActionOpenDoorsStageMessage();
+                return;
+            }
+
+            if (this.stateManagementInstance.AlreadyKilledZombie() && this.inventoryInstance.HasKey() && !this.stateManagementInstance.AlreadyUsedKey())
+            {
+                this.ShowPostActionWithKeyStageMessage();
+                return;
+            }
+
             if (this.stateManagementInstance.AlreadyKilledZombie())
             {
                 this.ShowPostActionStageMessage();
@@ -139,6 +164,48 @@ namespace Reign_of_Grelok.stages
             Console.ReadKey();
             Console.Clear();
             this.stateManagementInstance.SeeChapel();
+        }
+
+        private void ShowPostActionWithKeyStageMessage()
+        {
+            Console.Clear();
+            Console.WriteLine("Você olha ao seu redor...\n\n");
+            Console.WriteLine(
+                "Você está no final de um caminho de terra, de frente para uma pequena capela. " +
+                "As paredes de estuque estão desbotadas, faltam muitas telhas. " +
+                "As grandes portas de carvalho estão trancadas. " +
+                "A congregação não está em lugar nenhum. " +
+                "Um pequeno cemitério de lápides tortas fica à sombra do campanário rachado. " +
+                "O caminho de terra serpenteia para oeste através de uma planície grande e indefinida.\r\n\r\n" +
+                "Você usa a chave dada pelo padre, e agora, as portas da capela estão abertas.\r\n\r\n" +
+                "Há uma cova aberta nas proximidades."
+            );
+            Console.WriteLine();
+            Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+            this.stateManagementInstance.SeeChapel();
+            this.stateManagementInstance.UseKey();
+        }
+
+        private void ShowPostActionOpenDoorsStageMessage()
+        {
+            Console.Clear();
+            Console.WriteLine("Você olha ao seu redor...\n\n");
+            Console.WriteLine(
+                "Você está no final de um caminho de terra, de frente para uma pequena capela. " +
+                "As paredes de estuque estão desbotadas, faltam muitas telhas. " +
+                "As grandes portas de carvalho estão trancadas. " +
+                "A congregação não está em lugar nenhum. " +
+                "Um pequeno cemitério de lápides tortas fica à sombra do campanário rachado. " +
+                "O caminho de terra serpenteia para oeste através de uma planície grande e indefinida.\r\n\r\n" +
+                "As portas da capela estão abertas.\r\n\r\n" +
+                "Há uma cova aberta nas proximidades."
+            );
+            Console.WriteLine();
+            Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
         }
 
         private void ShowGraveMessage()
@@ -207,6 +274,23 @@ namespace Reign_of_Grelok.stages
             Console.ReadKey();
             Console.Clear();
             this.stateManagementInstance.KillZombie();
+        }
+
+        private void CheckChapel()
+        {
+            Console.Clear();
+            Console.WriteLine("Você entra na capela vazia...\n\n");
+            Console.WriteLine();
+            Console.WriteLine(
+                "Partículas de poeira pendem preguiçosamente nos raios de luz colorida que se estendem pelas janelas pontiagudas pela capela. " +
+                "Os bancos, o púlpito e tudo o mais estão cobertos por uma névoa fina. " +
+                "Perto da entrada existe uma cisterna de pedra muito profunda. " +
+                "Está cheio até a borda com água benta."
+            );
+            Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+            this.inventoryInstance.FulfillFlask();
         }
     }
 }
