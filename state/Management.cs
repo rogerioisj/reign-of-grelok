0aa011eeeef5859ad7ef8eaae46f9f7a887e3bef
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-07-24 18:28:41 -0300

    feat: add states for chapel scenario

--- state/Management.cs
+++ state/Management.cs
@@ -4,17 +4,29 @@ namespace Reign_of_Grelok.state
 {
     class Management
     {
-        private bool hasSeeTown;
+        private bool hasSawTown;
+        private bool hasSawChapel;
+        private bool hasKilledZombie;
         private bool hasFinishedGame;
         public Management() {
-            hasSeeTown = false;
+            hasSawTown = false;
+            hasSawChapel = false;
+            hasKilledZombie = false;
             hasFinishedGame = false;
         }
 
-        public void SeeTown() { this.hasSeeTown = true; }
+        public void SeeTown() { this.hasSawTown = true; }
 
-        public bool AlreadyCheckTown() { return this.hasSeeTown; }
+        public void SeeChapel() { this.hasSawChapel = true; }
+
+        public void KillZombie() { this.hasKilledZombie = true; }
 
         public void FinishGame() { this.hasFinishedGame = true; }
+
+        public bool AlreadyCheckTown() { return this.hasSawTown; }
+
+        public bool AlreadyCheckChapel() { return this.hasSawChapel; }
+
+        public bool AlreadyKilledZombie() { return this.hasKilledZombie; }
     }
 }
