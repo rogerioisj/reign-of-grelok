7e5cb31be7926213dd4cd477e0fc35479dec4c2a
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-07-25 15:05:29 -0300

    feat: add montain state

--- state/Management.cs
+++ state/Management.cs
@@ -7,15 +7,21 @@ namespace Reign_of_Grelok.state
         private bool hasSawTown;
         private bool hasSawChapel;
         private bool hasSawSwamp;
+        private bool hasSawMontainside;
         private bool hasTalkedToWizard;
         private bool hasKilledZombie;
         private bool hasUsedKey;
+        private bool hasTakedGem;
         private bool hasFinishedGame;
         public Management() {
             hasSawTown = false;
             hasSawChapel = false;
+            hasSawSwamp = false;
+            hasSawMontainside = false;
+            hasTalkedToWizard = false;
             hasKilledZombie = false;
             hasUsedKey = false;
+            hasTakedGem = false;
             hasFinishedGame = false;
         }
 
@@ -25,9 +31,13 @@ namespace Reign_of_Grelok.state
 
         public void SeeSwamp() { this.hasSawSwamp = true; }
 
+        public void SeeMontainside() { this.hasSawMontainside = true; }
+
         public void KillZombie() { this.hasKilledZombie = true; }
 
         public void UseKey() { this.hasUsedKey = true; }
+
+        public void GetRawGem() { this.hasTakedGem = true; }
 
         public void TalkToWizard() {  this.hasTalkedToWizard = true; }
 
@@ -39,11 +49,15 @@ namespace Reign_of_Grelok.state
 
         public bool AlreadyCheckSwamp() { return this.hasSawSwamp; }
 
+        public bool AlreadyCheckMontainside() { return this.hasSawMontainside; }
+
         public bool AlreadyKilledZombie() { return this.hasKilledZombie; }
 
         public bool AlreadyUsedKey() { return this.hasUsedKey; }
 
         public bool AlreadyTalkedToWizard() { return this.hasTalkedToWizard; }
+
+        public bool AlreadyTakedGem() { return this.hasTakedGem; }
 
 
     }
