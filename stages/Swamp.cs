7697433e0829a94b57d3884ddfd29043427f2747
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-07-25 14:26:15 -0300

    feat: add text for wizard talk

--- stages/Swamp.cs
+++ stages/Swamp.cs
@@ -21,10 +21,9 @@ namespace Reign_of_Grelok.stages
             Console.WriteLine("------------------------------------");
             Console.WriteLine("Pressione de acordo com o destino:");
             Console.WriteLine("1 - Olhar ao redor");
-            if (stateManagementInstance.AlreadyCheckTown())
+            if (stateManagementInstance.AlreadyCheckSwamp())
             {
-                Console.WriteLine("2 - Falar com ferreiro");
-                Console.WriteLine("3 - Falar com padre");
+                Console.WriteLine("2 - Falar com mago");
             }
             Console.WriteLine("4 - Ir para Leste");
             Console.WriteLine("I - Iventário");
@@ -41,6 +40,14 @@ namespace Reign_of_Grelok.stages
             {
                 case '1':
                     this.ShowStageMessage();
+                    this.Load(callback);
+                    break;
+                case '2':
+                    this.CheckIfOptionIsAvailable(
+                        this.stateManagementInstance.AlreadyCheckSwamp(),
+                        callback
+                        );
+                    this.ShowWizardMessage();
                     this.Load(callback);
                     break;
                 case '4':
@@ -80,13 +87,63 @@ namespace Reign_of_Grelok.stages
                 "Uma varanda se projeta no meio da face da torre. " +
                 "Os cheiros inebriantes do incenso misturam-se com o fedor nauseante do pântano. " +
                 "O caminho de pedra desenrola-se para leste, em direção a uma ampla planície além dos pântanos.\r\n\r\n" +
-                "Um bruxo está aqui, gesticulando loucamente em sua varanda."
+                "Um mago está aqui, gesticulando loucamente em sua varanda."
             );
             Console.WriteLine();
             Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
             Console.ReadKey();
             Console.Clear();
-            this.stateManagementInstance.SeeChapel();
+            this.stateManagementInstance.SeeSwamp();
+        }
+
+        private void CheckIfOptionIsAvailable(bool option, CallbackStageMenu callback)
+        {
+            if (!option)
+            {
+                Console.Clear();
+                Console.WriteLine("Opção inválida!\n\n\n");
+                this.Load(callback);
+            }
+        }
+
+        private void ShowWizardMessage()
+        {
+            if (this.stateManagementInstance.AlreadyTalkedToWizard())
+            {
+                this.ShowWizardPostActionMessage();
+            }
+
+            this.ShowWizardStandardMessage();
+        }
+
+        private void ShowWizardStandardMessage()
+        {
+            Console.Clear();
+            Console.Write("Você fala com o mago...\n\n\n");
+            Console.WriteLine(
+                "O mago acena freneticamente para você da varanda dele. “Você está aqui, você chegou!”, ele exclama. " +
+                "Depois de um silêncio constrangedor, ele enfia o dedo excitado em uma bola de cristal, quase derrubando-a no pântano.\r\n\r\n\"" +
+                "Eu vi, você vê. Você é quem derrotou Grelok. Hoo-hoo!\" O homenzinho pula no corrimão, girando uma pirueta. \"" +
+                "Agora chegou a hora de fazer minha parte. Jogue a joia!\"\r\n\r\nA testa do mago se franze. \"" +
+                "As coisas estão um pouco fora de ordem, não é? Volte quando tiver uma pedra preciosa poderosa. " +
+                "Em breve - nunca consegui cumprir uma profecia antes!\""
+            );
+            Console.WriteLine();
+            Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+            this.stateManagementInstance.TalkToWizard();
+        }
+
+        private void ShowWizardPostActionMessage()
+        {
+            Console.Clear();
+            Console.Write("Você fala com o mago...\n\n\n");
+            Console.WriteLine("O mago está enxotando você, com as mangas balançando.\r\n\r\n\"Vá! Encontre a pedra preciosa e volte, para que eu possa fazer minha parte!\"");
+            Console.WriteLine();
+            Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
