b7e06ca41398243d47a84b2ef8ba619a2486e0ac
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-07-25 14:25:40 -0300

    feat: add methods to manage swamp state

--- state/Management.cs
+++ state/Management.cs
@@ -6,6 +6,8 @@ namespace Reign_of_Grelok.state
     {
         private bool hasSawTown;
         private bool hasSawChapel;
+        private bool hasSawSwamp;
+        private bool hasTalkedToWizard;
         private bool hasKilledZombie;
         private bool hasUsedKey;
         private bool hasFinishedGame;
@@ -21,9 +23,13 @@ namespace Reign_of_Grelok.state
 
         public void SeeChapel() { this.hasSawChapel = true; }
 
+        public void SeeSwamp() { this.hasSawSwamp = true; }
+
         public void KillZombie() { this.hasKilledZombie = true; }
 
         public void UseKey() { this.hasUsedKey = true; }
+
+        public void TalkToWizard() {  this.hasTalkedToWizard = true; }
 
         public void FinishGame() { this.hasFinishedGame = true; }
 
@@ -31,9 +37,13 @@ namespace Reign_of_Grelok.state
 
         public bool AlreadyCheckChapel() { return this.hasSawChapel; }
 
+        public bool AlreadyCheckSwamp() { return this.hasSawSwamp; }
+
         public bool AlreadyKilledZombie() { return this.hasKilledZombie; }
 
         public bool AlreadyUsedKey() { return this.hasUsedKey; }
+
+        public bool AlreadyTalkedToWizard() { return this.hasTalkedToWizard; }
 
 
     }
